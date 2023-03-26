cfd2f90321e6dd6128147fb92545a208b922a0ff
George Castillo <gmcastil@gmail.com>
2023-03-26 10:11:54 -0400

    Added more messages about how to use the code I made here as an example of writing and reading to a slave peripheral

--- src/peripheral/peripheral_self_test.c
+++ src/peripheral/peripheral_self_test.c
@@ -1,6 +1,29 @@
 /*
  * Very basic check of an AXI4-Lite hardware peripheral based on Adam Taylor's
  * Microzed Chronicles Parts 21-27
+ *
+ * In brief, the hardware that supports this little endeavour was the default
+ * 4-register AXI4-Lite slave that is created from 'Tools -> Create and Package
+ * New IP'.  An important note is that I also have an XADC IP block on the same
+ * AXI bus, so the location for the newly created IP reflects that.  I won't try
+ * to recreate the address map here; presumably, if the reader of this comment
+ * is sufficiently switched on to have gotten this far, they will understand
+ * what needs to be changed (i.e., base address specified by the TaylorPtr
+ * value) to get it to point at the correct place.
+ *
+ * Note also that, if you're using the Xilinx AXI4-Lite slave logic, it will
+ * almost certainly not work in any sort of real application.  The Xilinx AXI
+ * bus treatment is known to be garbage and non-compliant in a lot of cases, so
+ * just because their driver code (which is equally trashy) returns an
+ * XST_SUCCESS or whatever, does not mean that the underlying transaction was
+ * serviced appropriately.  Include ILA cores at relevant locations with all
+ * signals of interest to verify that things are actually happening.  As an
+ * example, one can write to all manner of locations in the peripheral address
+ * space and receive success indicators, but no actual hardware exists at that
+ * location.
+ *
+ * If you blindly trust the Xilinx hardware implementations, you will eventually
+ * get burned.  You've been warned.
  */
 
 #include <stdio.h>
