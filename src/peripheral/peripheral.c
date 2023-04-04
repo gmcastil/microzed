90e9fcba03ce3f7999bd322835fa1d88c28059c0
George Castillo <gmcastil@gmail.com>
2023-04-04 08:24:16 -0400

    Working example of the altitude to pressure calculations that Taylor makes in Microzed Chronicles sections on PL peripherals.
    - The PS/PL example Adam uses computes altitude vs pressure in mbar
    using a quadratic equation that is performed in software and then timed
    using one of the CPU private timers. He then dumps out some poorly
    formatted summary output to show that it takes an average of 25
    CPU_3x2x clocks to calculate the result.
    - I get something similar, however both of us get a significantly
    different result the firt time that the function gets called.  Adam got
    89 clocks on his first conversion, and then 25 on the subsequent calls.
    - My result is similar, but different because I'm actually using a
    function call to do it, so I am assuming that the cycle differences are
    due to overhead required to call the function itself.  I'm going to
    rewrite it using a macro instead of the function call so that I can see
    the difference in overhead.
    - My suspicion is that I will see a loss of 5-6 clock cycles due to
    removal of the function call overhead.  The difference between the first
    and later calls has me stumped though.  I might actually need to step
    through the assembly language generated by the compiler to see what's
    going on.

--- src/peripheral/peripheral.c
+++ src/peripheral/peripheral.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 #include <inttypes.h>
 
@@ -6,27 +7,27 @@
 #include "xscugic.h"
 #include "xil_exception.h"
 
-/* Xilinx triple timer counter libraries */
-#include "xttcps.h"
-#include "platform.h"
+/* Xilinx private timer driver */
+#include "xscutimer.h"
 
 /* Debug and workaround codes */
-#include "ttc_dbg.h"
 #include "ps7_dbg.h"
+
+#include "taylor_uzed.h"
 
 /* Necessary for creating driver instances */
-#define GIC_DEVICE_ID                           XPAR_SCUGIC_SINGLE_DEVICE_ID
-#define TTC_DEVICE_ID                           XPAR_XTTCPS_0_DEVICE_ID
+#define GIC_DEVICE_ID			XPAR_SCUGIC_SINGLE_DEVICE_ID
+#define TIMER_DEVICE_ID			XPAR_XSCUTIMER_0_DEVICE_ID
 
-/*
- * This defines the interrupt that the GIC will see, but it could be for several different
- * kinds of interrupts on that particular counter (e.g., match, interval, overflow)
- */
-#define TTC0_IRQ_ID                                     XPS_TTC0_0_INT_ID
+/* Hardware peripheral definitions */
+#define PERIPHERAL_BASE			0x43C10000
+#define PERIPHERAL_SCRATCH_OFFSET	0x04
 
-#define ASCII_ESC                                       27
+#define PERIPHERAL_ADD			0x01
+#define PERIPHERAL_SUB			0x02
+#define PERIPHERAL_MULT			0x03
 
-#include "ps7_dbg.h"
+#define ASCII_ESC		27
 
 void clear_console()
 {
@@ -36,7 +37,7 @@ void clear_console()
 }
 void print_operation(char *str)
 {
-        fprintf(stdout, "%-50s", str);
+        fprintf(stdout, "%-30s", str);
         fflush(stdout);
 }
 void print_result(char *str)
@@ -44,17 +45,135 @@ void print_result(char *str)
         fprintf(stdout, "%10s\n", str);
         fflush(stdout);
 }
+void print_diff(uint32_t actual, uint32_t expected)
+{
+		fprintf(stdout, "Actual: 0x%08"PRIx32" Expected: 0x%08"PRIx32"\n",
+				(uint32_t) actual, (uint32_t) expected);
+		fflush(stdout);
+}
+
+uint32_t peripheral_scratch(uint32_t *p_ptr, uint32_t val)
+{
+	Xil_Out32((uint32_t) p_ptr + PERIPHERAL_SCRATCH_OFFSET, val);
+	return Xil_In32((uint32_t) p_ptr + PERIPHERAL_SCRATCH_OFFSET);
+}
+
+void print_summary(uint32_t *p_ptr, int NumRegs)
+{
+        uint32_t value = 0;
+        uint32_t *ptr = p_ptr;
+
+        int i = 0;
+
+        for (i = 0; i < NumRegs; i++) {
+                value = Xil_In32((uint32_t) ptr);
+                printf("Addr: %8p Data: 0x%08"PRIx32"\n", ptr++, value);
+        }
+
+        return;
+}
+
+uint32_t peripheral_add(uint32_t *p_ptr, uint32_t add_0, uint32_t add_1)
+{
+	TAYLOR_UZED_mWriteReg((uint32_t) p_ptr, TAYLOR_UZED_S00_AXI_SLV_REG1_OFFSET, add_0);
+	TAYLOR_UZED_mWriteReg((uint32_t) p_ptr, TAYLOR_UZED_S00_AXI_SLV_REG2_OFFSET, add_1);
+	TAYLOR_UZED_mWriteReg((uint32_t) p_ptr, TAYLOR_UZED_S00_AXI_SLV_REG0_OFFSET, PERIPHERAL_ADD);
+
+	return TAYLOR_UZED_mReadReg((uint32_t) p_ptr, TAYLOR_UZED_S00_AXI_SLV_REG3_OFFSET);
+}
+
+uint32_t peripheral_subtract(uint32_t *p_ptr, uint32_t add_0, uint32_t add_1)
+{
+	TAYLOR_UZED_mWriteReg((uint32_t) p_ptr, TAYLOR_UZED_S00_AXI_SLV_REG1_OFFSET, add_0);
+	TAYLOR_UZED_mWriteReg((uint32_t) p_ptr, TAYLOR_UZED_S00_AXI_SLV_REG2_OFFSET, add_1);
+	TAYLOR_UZED_mWriteReg((uint32_t) p_ptr, TAYLOR_UZED_S00_AXI_SLV_REG0_OFFSET, PERIPHERAL_SUB);
+
+	return TAYLOR_UZED_mReadReg((uint32_t) p_ptr, TAYLOR_UZED_S00_AXI_SLV_REG3_OFFSET);
+}
+
+uint32_t peripheral_multiply(uint32_t *p_ptr, uint32_t add_0, uint32_t add_1)
+{
+	TAYLOR_UZED_mWriteReg((uint32_t) p_ptr, TAYLOR_UZED_S00_AXI_SLV_REG1_OFFSET, add_0);
+	TAYLOR_UZED_mWriteReg((uint32_t) p_ptr, TAYLOR_UZED_S00_AXI_SLV_REG2_OFFSET, add_1);
+	TAYLOR_UZED_mWriteReg((uint32_t) p_ptr, TAYLOR_UZED_S00_AXI_SLV_REG0_OFFSET, PERIPHERAL_MULT);
+
+	return TAYLOR_UZED_mReadReg((uint32_t) p_ptr, TAYLOR_UZED_S00_AXI_SLV_REG3_OFFSET);
+}
+
+/* Try a function first and then use a macro instead to see the difference it makes */
+float altitude_to_pressure(float a, float b, float c, float x)
+{
+	/* Simple quadratic equation */
+	return (a*(x*x)) + (b*x) + c;
+}
 
 int main(int args, char *argv[])
 {
 
-	init_platform();
+	float i = 0;
+	uint32_t start_time = 0;
+	uint32_t stop_time = 0;
+
+	float result = 0;
+	uint32_t scratch32;
+
+	XScuTimer_Config *timer_config = NULL;
+	XScuTimer *timer = NULL;
 
 	clear_console();
 
+	/* ------------ PS interface check  --------------------------------------- */
 	ps7_dbg_sanity_check();
 
-    cleanup_platform();
+	/* ------------ Check that peripheral is functioning ---------------------- */
+	print_operation("Scratch register");
+	uint32_t *p_ptr = (uint32_t *) PERIPHERAL_BASE;
+	/* Just going to use pi here as a scratch value */
+	scratch32 = peripheral_scratch(p_ptr, 3141);
+	if ( 3141 != scratch32 ) {
+		print_result("FAIL");
+		print_diff(scratch32, 3141);
+	} else {
+		print_result("OK");
+	}
+
+	/* ------------ Configuring private timer  --------------------------------- */
+	print_operation("Setting up timer");
+	timer_config = XScuTimer_LookupConfig(TIMER_DEVICE_ID);
+	if ( timer_config == NULL ) {
+		print_result("FAIL");
+		printf("Could not initialize private timer %d\n", TIMER_DEVICE_ID);
+	} else {
+		timer = malloc(sizeof(XScuTimer));
+		if ( timer == NULL ) {
+			print_result("FAIL");
+			printf("Could not allocate memory for private timer %d\n", TIMER_DEVICE_ID);
+		} else {
+			XScuTimer_CfgInitialize(timer, timer_config, timer_config->BaseAddr);
+			print_result("OK");
+		}
+	}
+
+	/* Private timer clock is always CPU_3X2X clock (333 or 250MHz) */
+	printf("\n");
+	printf("%-10s%-15s%-15s\n", "mbar", "altitude", "elapsed");
+	printf("%-10s%-15s%-15s\n", "----", "----------", "-------");
+	XScuTimer_SetPrescaler(timer, 0);
+	XScuTimer_DisableAutoReload(timer);
+	for (i = 0; i < 10.0; i = i + 0.1) {
+		XScuTimer_LoadTimer(timer, 0xFFFFFFFF);
+		start_time = XScuTimer_GetCounterValue(timer);
+		if ( start_time != 0xFFFFFFFF ) {
+			printf("Could not set start timer value\n");
+		}
+		XScuTimer_Start(timer);
+		result = altitude_to_pressure((-0.0088), (1.7673), (131.29), i);
+		XScuTimer_Stop(timer);
+		stop_time = XScuTimer_GetCounterValue(timer);
+		printf("%-10.2f%-15.6f%-12"PRIu32"\n", i, result, start_time - stop_time);
+	}
+
+	free(timer);
 
 	return 0;
 }
